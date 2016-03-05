1f6bcad14a64a897166137473d136e3df1d772c2
tsenggordon <kuoweitseng@gmail.com>
2016-03-05 12:15:07 -0500

    data split

---
+++ data_preprocess.py
@@ -0,0 +1,63 @@
+import csv, random
+import numpy as np
+DATA_PATH = "../data/" 
+TRAIN_FILE = "train.txt" #45840617 lines
+def partitioning(f_name):
+	head, ext = TRAIN_FILE.split(".")
+	data = []
+	idx_10M, idx_5M, idx_2M, idx_3M = genIndices(f_name)
+	train_10M = DATA_PATH + head + "_10M.csv"
+	train_5M = DATA_PATH + head + "_5M.csv"
+	train_2M = DATA_PATH + head + "_2M.csv"
+	train_3M = DATA_PATH + head + "_3M.csv"
+	with open(f_name) as f:
+		f_10M = open(train_10M, "wb")
+		f_5M = open(train_5M, "wb")
+		f_2M = open(train_2M, "wb")
+		f_3M = open(train_3M, "wb")
+		reader = csv.reader(f)
+		w_10M = csv.writer(f_10M)
+		w_5M = csv.writer(f_5M)
+		w_2M = csv.writer(f_2M)
+		w_3M = csv.writer(f_3M)
+		for idx, row in enumerate(reader):
+			if idx*100/45840617 % 1 == 0:
+				print idx*100/45840617, "%"
+			if idx_10M and idx == idx_10M[0]:
+				idx_10M.pop()
+				w_10M.writerow(row)
+				if idx_5M and idx == idx_5M[0]:
+					idx_5M.pop()
+					w_5M.writerow(row)
+				if idx_2M and idx == idx_2M[0]:
+					idx_2M.pop()
+					w_2M.writerow(row)
+				if idx_3M and idx == idx_3M[0]:
+					idx_3M.pop()
+					w_3M.writerow(row)
+		f_10M.close()
+		f_5M.close()
+		f_2M.close()
+		f_3M.close()
+		#print len(data), len(data[0])
+def lineOfFile(f_name):
+	with open(f_name) as f:
+		num_lines = sum(1 for line in f)
+	print " has %d line" %num_lines
+def genIndices(f_name):
+	#num_lines = lineOfFile(f_name)
+	print "gen indices"
+	num_lines = 45840617
+	idx_10M = np.random.permutation(45840617)[:10000000].tolist()
+	idx_5M = idx_10M[:5000000]
+	idx_2M = idx_10M[5000000:7000000]
+	idx_3M = idx_10M[7000000:]
+	idx_10M.sort()
+	idx_5M.sort()
+	idx_2M.sort()
+	idx_3M.sort()
+	print "gen indices finished"
+	return idx_10M, idx_5M, idx_2M, idx_3M
+partitioning(DATA_PATH+TRAIN_FILE)
+#lineOfFile(DATA_PATH+TRAIN_FILE)
+#print np.random.permutation(10).tolist()
